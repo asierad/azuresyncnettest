e9a2c144e3153fecda17fb43b1c20b8096703493
Adrian Sieradzki <sarcouy@gmail.com>
2015-05-25 13:53:17 -0300

    Second attempt

---
+++ TestApp/TestApp/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestApp.Startup))]
+namespace TestApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
